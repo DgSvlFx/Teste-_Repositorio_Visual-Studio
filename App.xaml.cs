035e24510e1efd1da28163aaa2509eb78f5fe39a
diogo <diogo@DS>
2024-03-11 15:06:44 +0000

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Teste_Repositorio
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
